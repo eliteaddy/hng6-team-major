14403c77a258a51d9e271aef6e9b9f832b631d4b
Haryormeedey <adesileisaiah70@gmail.com>
2019-09-18 17:14:11 +0100

    Updates

---
+++ precache-manifest.4cccd1ecfaa93aeb1037da95dc44714a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8321b9c1d814d1bc4dc4012965a91b4",
+    "url": "/hng6-team-major/index.html"
+  },
+  {
+    "revision": "4c3915fc32fed6f79b3c",
+    "url": "/hng6-team-major/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "d6a02078e21dcca6dfb9",
+    "url": "/hng6-team-major/static/js/2.2e4ca723.chunk.js"
+  },
+  {
+    "revision": "4c3915fc32fed6f79b3c",
+    "url": "/hng6-team-major/static/js/main.dcd1e5cc.chunk.js"
+  },
+  {
+    "revision": "2afb2be12e1da99f9f24",
+    "url": "/hng6-team-major/static/js/runtime~main.5cc6c67b.js"
+  }
+]);
